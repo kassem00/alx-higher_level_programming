8576ffc52fd16c966958146a1f439b0f941d38d0
kassem00 <kamsdonga@gmail.com>
2023-08-12 18:53:59 +0300

    au

--- 0x02-python-import_modules/my_fun.py
+++ 0x02-python-import_modules/my_fun.py
@@ -2,9 +2,8 @@
 # my_fun.py
 import sys
 
-
 def fun():
-	"""loop through arguments"""
+    """loop through arguments"""
 	for i in range(len(sys.argv)):
             if i > 0:
                 print(f"{i}: {sys.argv[i]}")
