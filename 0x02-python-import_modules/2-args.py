ba4d1076b7c733fb43141414fdb22febba73dc0a
kassem00 <kamsdonga@gmail.com>
2023-08-12 18:22:04 +0300

    au

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,14 +5,13 @@ if __name__ == "__main__":
     import sys
     import my_fun
 
+    counter = len(sys.argv) - 1
 
-counter = len(sys.argv) - 1
-
-if counter == 0:
-    print("0 arguments.")
-elif counter == 1:
-    print("1 argument:")
-    my_fun.fun()
-else:
-    print("{} arguments:".format(count))
-    my_fun.fun()
+    if counter == 0:
+        print("0 arguments.")
+    elif counter == 1:
+        print("1 argument:")
+        my_fun.fun()
+    else:
+        print("{} arguments:".format(counter))
+        my_fun.fun()
--- 0x02-python-import_modules/my_fun.py
+++ 0x02-python-import_modules/my_fun.py
@@ -1,13 +1,10 @@
 #!/usr/bin/python3
 # my_fun.py
-
-if __name__ == "__main__":
-    """loop througe argument"""
-    import sys
+import sys
 
 
 def fun():
-    """loop througe argument"""
-    for i in range(len(sys.argv)):
-        if i > 0:
-            print(i, sys.argv[i])
+	"""loop through arguments"""
+	for i in range(len(sys.argv)):
+            if i > 0:
+                print(i, sys.argv[i])
