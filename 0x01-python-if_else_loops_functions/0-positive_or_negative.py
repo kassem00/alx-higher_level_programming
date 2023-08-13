87764cc9c05306e2ed8b199fc23cb56ec0b364f8
kassem00 <kamsdonga@gmail.com>
2023-08-13 21:02:07 +0300

    au

--- 0x01-python-if_else_loops_functions/0-positive_or_negative.py
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -13,7 +13,7 @@ followed by a new line
 
 if number < 0:
     print("{} is positive".format(number))
-elif: number == 0:
+elif number == 0:
     print("{} is zero".format(number))
 else:
     print("{} is negative".format(number))
