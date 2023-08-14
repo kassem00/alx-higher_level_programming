157f0cfe6fcb81ea7dd97527c5e3aafdaddf2c2b
kassem00 <kamsdonga@gmail.com>
2023-08-14 03:34:16 +0300

    au

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -13,11 +13,11 @@ if __name__ == "__main__":
     followed by the argument value and a new line
     '''
     num_args = len(argv) - 1
-    print("{} argument{}".format(num_args, 's' if num_args < 1 else ''),
+    print("{} argument{}".format(num_args, 's' if num_args != 1 else ''),
           end='')
     if num_args == 0:
         print(".")
     else:
-        print()
+        print(':')
         for i in range(1, len(argv)):
             print("{}: {}".format(i, argv[i]))
