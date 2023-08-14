f9ed0360dea22ded154fc4e3565b2e50883e955d
kassem00 <kamsdonga@gmail.com>
2023-08-14 03:25:06 +0300

    au

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,7 +4,7 @@ from sys import argv
 
 if __name__ == "__main__":
     num_args = len(argv) - 1
-    print("{} argument{}:".format(num_args, 's' if num_args != 1 else ''),
+    print("{} argument{}".format(num_args, 's' if num_args < 1 else ''),
           end='')
     if num_args == 0:
         print(".")
