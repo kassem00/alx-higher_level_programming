72eb3409df78f52000db430cf572cf1b3c2312a8
kassem00 <kamsdonga@gmail.com>
2023-08-14 06:10:27 +0300

    au

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,11 +1,9 @@
-import hidden_4
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import hidden_4
 
-def print_all_names():
     names = dir(hidden_4)
     for name in names:
         if name[:2] != "__":
             print(name)
 
-if name == "main":
-    """Print all names defined by hidden_4 module."""
-    print_all_names()
