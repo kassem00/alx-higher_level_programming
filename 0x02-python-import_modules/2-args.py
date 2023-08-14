2063d30a0c3bf84a62ac010e6872b1ddd50c89fe
kassem00 <kamsdonga@gmail.com>
2023-08-14 03:29:20 +0300

    au

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,6 +3,15 @@
 from sys import argv
 
 if __name__ == "__main__":
+    '''
+    Number of argument(s) followed by argument
+    (if number is one) or arguments (otherwise), followed by
+    : (or . if no arguments were passed) followed by
+    a new line, followed by (if at least one argument),
+    one line per argument:
+    the position of the argument (starting at 1) followed by :,
+    followed by the argument value and a new line
+    '''
     num_args = len(argv) - 1
     print("{} argument{}".format(num_args, 's' if num_args < 1 else ''),
           end='')
