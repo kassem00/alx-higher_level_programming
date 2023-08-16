df31def7874a18872677897e09221a3ede6db092
kassem00 <kamsdonga@gmail.com>
2023-08-16 22:16:42 +0300

    au

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -4,4 +4,6 @@ def no_c(my_string):
     str_var = '' + my_string
     str_var = my_string.lstrip('c')
     str_var = my_string.lstrip('C')
+    str_var = my_string.strip('c')
+    str_var = my_string.strip('C')
     return str_var
