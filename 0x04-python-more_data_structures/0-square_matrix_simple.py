dcaf2d1d99b8f023086328baf592aa49b7142740
kassem00 <kamsdonga@gmail.com>
2023-08-19 04:40:56 +0300

    au

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
 
 def square_matrix_simple(matrix=[]):
-    return [[x * x for x in row] for row in matrix]
+    c_mat = matrix
+
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+            c_mat[i][j] = c_mat[i][j] * c_mat[i][j]
+    return c_mat
