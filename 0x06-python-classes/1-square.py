8faf2c58416b04d575a97c635c47a135ce9b614c
kassem00 <kamsdonga@gmail.com>
2023-08-30 14:53:33 +0300

    au

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,2 +1,8 @@
 #!/usr/bin/python3
+'print(__import__("my_module").my_function.__doc__)'
 
+
+class Square:
+    'print(__import__("my_module").my_function.__doc__)'
+    def __init__(self, size):
+        self.__size = size
