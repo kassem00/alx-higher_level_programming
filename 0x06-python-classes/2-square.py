e7950ec94a350bf9c8763309cbca9c7dc37878a4
kassem00 <kamsdonga@gmail.com>
2023-08-30 15:06:56 +0300

    au

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,2 +1,16 @@
 #!/usr/bin/python3
+'print(__import__("my_module").my_function.__doc__)'
 
+
+class Square:
+    'print(__import__("my_module").my_function.__doc__)'
+    def __init__(self, size=0):
+        self.__size = size
+        try:
+            pass
+        if type(size) != int:
+                print("size must be an integer")
+                return TypeError
+        if size < 0:
+            print("TypeError")
+            return ValueError
