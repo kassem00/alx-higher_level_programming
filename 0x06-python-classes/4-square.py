ef407acd6ec8d7a65d4ce3858b00b7f8c20b4c8b
kassem00 <kamsdonga@gmail.com>
2023-08-30 17:48:53 +0300

    au

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,2 +1,23 @@
 #!/usr/bin/python3
+'print(__import__("my_module").my_function.__doc__)'
 
+
+class Square:
+    'print(__import__("my_module").my_function.__doc__)'
+    def __init__(self, size=0):
+        self.size
+
+    @property
+    def size(self):
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        return (self.__size * self.__size)
