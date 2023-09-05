991079045bee996bdc3b5e904011647c70d33b3f
kassem00 <kamsdonga@gmail.com>
2023-09-05 19:33:41 +0300

    au

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -7,6 +7,7 @@ class Rectangle:
     """rectangle."""
     number_of_instances = 0
     print_symbol = "#"
+
     def __init__(self, width=0, height=0):
         """Initialize a Rectangle."""
         type(self).number_of_instances += 1
