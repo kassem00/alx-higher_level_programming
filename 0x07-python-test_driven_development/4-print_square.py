9fd71fb5a56079037e838cd59dfa3a94bceb9a41
kassem00 <kamsdonga@gmail.com>
2023-09-08 22:04:06 +0300

    au

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+'''a function that adds 2 integers.'''
+
+def print_square(size):
+    ''' function that prints a square with the character # '''
+    if type(size) == float and size < 0:
+        raise TypeError("size must be an integer")
+    elif size < 0:
+        raise ValueError("size must be >= 0")
+    for i in range(0, size):
+        for j in range(size):
+            print("#", end="")
+        print("")
