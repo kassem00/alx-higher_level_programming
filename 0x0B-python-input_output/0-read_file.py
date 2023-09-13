3088b8c6ec54b4cc045f1dccc8b615cd732b4d6a
kassem00 <kamsdonga@gmail.com>
2023-09-13 09:20:04 +0000

    au

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -3,5 +3,6 @@
 
 
 def read_file(filename=""):
+    """function that reads a text file (UTF8) and prints it to stdout"""
     with open(filename, encoding="utf-8") as op_file:
         print(op_file.read())
