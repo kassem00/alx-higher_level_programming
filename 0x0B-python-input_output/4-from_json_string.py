d898e8356fa62b56d461e67203327f166ea3d2ff
kassem00 <kamsdonga@gmail.com>
2023-09-13 16:09:36 +0000

    au

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+'''1. write file'''
+import json
+
+
+def to_json_string(my_obj):
+    """function that returns the JSON representation of an object (string):"""
+    return json.dumps(my_obj)
