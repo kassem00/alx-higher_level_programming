81d0f44667f7bb7a664e83c64eee268b9f85c15c
kassem00 <kamsdonga@gmail.com>
2023-09-13 16:28:01 +0000

    au

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -3,6 +3,6 @@
 import json
 
 
-def to_json_string(my_obj):
-    """Return the JSON representation of a string object."""
-    return json.dumps(my_obj)
+def from_json_string(my_str):
+    """Return the Python object representation of a JSON string."""
+    return json.loads(my_str)
