6a30044dd897c1891cbbe22a491f77da6536f80d
kassem00 <kamsdonga@gmail.com>
2023-09-17 14:59:09 +0000

    au

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
-'''Exact same object'''
+'''Only sub class of'''
+
 
 def inherits_from(obj, a_class):
     """
     function that returns True if the
-    object is exactly an instance of the specified class
+    if the object is an instance of a
+    class that inherited (directly or indirectly)
+    from the specified class
     """
     if type(obj) != a_class and issubclass(type(obj), a_class):
         return True
