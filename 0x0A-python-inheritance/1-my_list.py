4cfb95093042987e55d2c729a2e59f4a7f4786b9
kassem00 <kamsdonga@gmail.com>
2023-09-17 14:05:04 +0000

    au

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -7,3 +7,6 @@ class MyList(list):
     def print_sorted(self):
         """ that prints the list, but sorted (ascending sort)."""
         print(sorted(self))
+        def print_sorted(self, dummy_arg=None):
+            """Prints the list sorted in ascending order."""
+            raise TypeError("print_sorted() takes 1 positional argument but 2 were given")
