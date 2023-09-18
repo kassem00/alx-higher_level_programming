423b637139123af348b1ceffe385198ec3645f32
kassem00 <kamsdonga@gmail.com>
2023-09-18 23:46:55 +0000

    au

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -28,4 +28,75 @@ class Rectangle(Base):
         self.height = height
         self.x = x
         self.y = y
+
+    @property
+    def width(self):
+        """property of width"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Setter of width"""
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
+        self.__width = value
+
+
+    @property
+    def height(self):
+        """property of height"""
+        return self.__height
+
+    @height.setter    
+    def height(self, value):
+        """setter of height"""
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value <= 0:
+            raise ValueError("height must be > 0")
         
+        self.__height = value
+
+
+    @property
+    def x(self):
+        """property of x"""
+        return self.__x
+
+    @x.setter    
+    def x(self, value):
+        """setter of x"""
+        if type(value) is not int:
+            raise TypeError("x must be an integer")
+        if value < 0:
+            raise ValueError("x must be > 0")
+        self.__x = value
+
+
+    @property
+    def y(self):
+        """property of y"""
+        return self.__y
+
+    @y.setter    
+    def y(self, value):
+        """setter of y"""
+        if type(value) is not int:
+            raise TypeError("y must be an integer")
+        if value < 0:
+            raise ValueError("y must be > 0")
+        self.__y = value
+
+    def area(self):
+        """area """
+        return self.width * self.height
+
+    def display(self):
+        """prints in stdout the Rectangle instance with the character #."""
+        if self.width == 0 or self.height == 0:
+            print("")
+        else:
+            for i in range(self.height):
+                print("#" * self.width)
