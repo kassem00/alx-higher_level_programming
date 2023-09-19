4527999a69d248c2b2279a6f3237e71ecca99085
kassem00 <kamsdonga@gmail.com>
2023-09-19 01:56:34 +0000

    au

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -95,10 +95,43 @@ class Rectangle(Base):
         if self.width == 0 or self.height == 0:
             print("")
         else:
+            [print("") for z in range(self.y)]
             for i in range(self.height):
+                print(" " * self.x, end ="")
                 print("#" * self.width)
 
     def __str__(self):
         return (f"[Rectangle] ({self.id}) {self.x}/{self.y} - {self.width}/\
 {self.height}"
                 )
+
+    def update(self, *args, **kwargs):
+        """Update the Rectangle.
+
+        Args:
+            *args (ints): New attribute values.
+                - 1st argument represents id attribute
+                - 2nd argument represents width attribute
+                - 3rd argument represent height attribute
+                - 4th argument represents x attribute
+                - 5th argument represents y attribute
+           **kwargs (dict): key/value pairs for attributes.
+       """
+        if args:
+            if len(args) > 0:
+                self.id = args[0] if args[0] is not None else self.id
+            if len(args) > 1:
+                self.width = args[1]
+            if len(args) > 2:
+                self.height = args[2]
+            if len(args) > 3:
+                self.x = args[3]
+            if len(args) > 4:
+                self.y = args[4]
+
+        elif kwargs:
+            self.id = kwargs.get("id", self.id)
+            self.width = kwargs.get("width", self.width)
+            self.height = kwargs.get("height", self.height)
+            self.x = kwargs.get("x", self.x)
+            self.y = kwargs.get("y", self.y)
