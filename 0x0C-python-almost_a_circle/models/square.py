615b74774830dc7b7813b8e7774663f269b6aba0
kassem00 <kamsdonga@gmail.com>
2023-09-20 22:00:56 +0000

    au

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -16,8 +16,7 @@ class Square(Rectangle):
             id (int): id of new square opject.
         """
         super().__init__(size, size, x, y, id)
-    
-    
+
     def __str__(self):
         """[Square] (<id>) <x>/<y> - <size>"""
         return (f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}")
@@ -25,10 +24,38 @@ class Square(Rectangle):
     @property
     def size(self):
         """property of height"""
-        return self.height
+        return self.width
 
     @size.setter
     def size(self, value):
-        """setter of height"""
-        self.width = value
         self.height = value
+        self.width = value
+        """setter of height"""
+
+    def update(self, *args, **kwargs):
+        """
+        Update the Square.
+
+        Args:
+            *args (ints): New attribute values.
+                - 1st argument represents id attribute
+                - 2nd argument represents size attribute
+                - 3rd argument represents x attribute
+                - 4th argument represents y attribute
+            **kwargs (dict): New key/value pairs of attributes.
+        """
+        if args:
+            if len(args) > 0:
+                self.id = args[0] if args[0] is not None else self.id
+            if len(args) > 1:
+                self.size = args[1]
+            if len(args) > 2:
+                self.x = args[2]
+            if len(args) > 3:
+                self.y = args[3]
+
+        elif kwargs:
+            self.id = kwargs.get("id", self.id)
+            self.size = kwargs.get("size", self.size)
+            self.x = kwargs.get("x", self.x)
+            self.y = kwargs.get("y", self.y)
