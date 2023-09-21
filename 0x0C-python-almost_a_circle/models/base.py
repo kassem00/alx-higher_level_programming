d490b76ce324450dca1addbc106ed9183cd86c53
kassem00 <kamsdonga@gmail.com>
2023-09-21 00:48:25 +0000

    au

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -34,4 +34,5 @@ class Base:
         """
         if list_dictionaries is None or list_dictionaries == []:
             return "[]"
-        return json.dumps(list_dictionaries)
+        else:
+            return json.dumps(list_dictionaries)
