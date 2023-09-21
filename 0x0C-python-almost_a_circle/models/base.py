8347864da7eef8bd414acb74cc90ef02e57696bd
kassem00 <kamsdonga@gmail.com>
2023-09-21 00:39:34 +0000

    au

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -14,17 +14,17 @@ class Base:
     __nb_objects = 0
 
     def __init__(self, id=None):
-      """
-      Initialize a new Base opject.
+        """
+        Initialize a new Base opject.
 
-      Args:
-          id (int): id of new Base opject.
-      """
-      if id == None:
-          Base.__nb_objects += 1
-          self.id = Base.__nb_objects
-      else:
-          self.id = id
+        Args:
+            id (int): id of new Base opject.
+        """
+        if id is None:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
+        else:
+            self.id = id
     def to_json_string(list_dictionaries):
         """
         standard formats for sharing data
