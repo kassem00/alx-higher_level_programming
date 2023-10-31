77c840a37bfa3cde3f1f99281fc739c960ba91f7
kassem00 <kamsdonga@gmail.com>
2023-10-31 01:53:03 +0000

    up

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,6 +1,21 @@
 #!/usr/bin/python3
-import calculator_1
-from sys import argv
-
 if __name__ == "__main__":
+    import calculator_1
+    from sys import argv
 
+    args = argv
+    cal = calculator_1
+
+    if len(argv) != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+
+    num1, num2 = int(args[1]), int(args[3])
+    opr = {"+": cal.add, "-": cal.sub, "*": cal.mul, "/": cal.div}
+    op = args[2]
+    
+    if op not in list(opr.keys()):
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
+    else:
+        print("{} {} {} = {}".format(args[1] , op, args[3], opr[op](num1, num2)))
