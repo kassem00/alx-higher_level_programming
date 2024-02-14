a8b762e0b8d1b4845749b66d5e38c957cf14ec0c
kassem00 <kamsdonga@gmail.com>
2024-02-14 11:32:01 +0000

    au

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,4 +1,5 @@
 #!/usr/bin/node
 // script that prints a message depending
+
 const count = process.argv.length;
 console.log(count === 2 ? 'No argument' : count === 3 ? 'Argument found' : 'Arguments found');
