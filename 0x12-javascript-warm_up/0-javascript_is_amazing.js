1ddb09fb34e265410d11182955cc0f92ce38f06e
kassem00 <kamsdonga@gmail.com>
2024-02-14 10:05:57 +0000

    au

--- 0x12-javascript-warm_up/0-javascript_is_amazing.js
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -1,11 +1,5 @@
 #!/usr/bin/node
-<<<<<<< HEAD
-// script that prints “JavaScript is amazing”:
-||||||| 4ca8b7a
-//script that prints “JavaScript is amazing”:
-=======
 // script that prints “aJavaScript is amazing”:
 
->>>>>>> refs/remotes/origin/master
 const myVar = 'JavaScript is amazing';
 console.log(myVar);
