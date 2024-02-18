ee7d5863f4897da1f96407b0f4025435c33be759
kassem00 <kamsdonga@gmail.com>
2024-02-18 23:04:28 +0000

    au

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+script that lists all states name start with N
+from the database hbtn_0e_0_usa
+"""
+import MySQLdb
+import sys
+arg = sys.argv
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    c = db.cursor()
+    c.execute("SELECT * FROM `states` ORDER BY 'id'")
+    for state in c.fetchall():
+        if state == sys.argv[4]:
+            print(state)
