b836d108e051a9fed99586898796ebc75bd6a9d2
kassem00 <kamsdonga@gmail.com>
2024-02-18 23:07:58 +0000

    au

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -10,7 +10,7 @@ arg = sys.argv
 if __name__ == "__main__":
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     c = db.cursor()
-    c.execute("SELECT * FROM `states` ORDER BY 'id'")
+    c.execute("SELECT * FROM `states` ORDER BY \
+    'id'WHERE BINARY `name` = '{}'".format(sys.argv[4]))
     for state in c.fetchall():
-        if state == sys.argv[4]:
             print(state)
