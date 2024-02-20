fea1fc709943fd5e418c40b59d00735aa9c20cc4
kassem00 <kamsdonga@gmail.com>
2024-02-20 16:01:36 +0000

    au

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -10,7 +10,8 @@ if __name__ == "__main__":
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     c = db.cursor()
 
-    query = "SELECT name FROM cities WHERE state_id=(SELECT id FROM states WHERE name=%s LIMIT 1)"
+    query = "SELECT name FROM cities WHERE state_id=(SELECT id FROM\
+    states WHERE name=%s LIMIT 1)"
     c.execute(query, (sys.argv[4],))
 
     cities = [city[0] for city in c.fetchall()]
