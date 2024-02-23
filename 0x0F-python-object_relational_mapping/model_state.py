381334f5bb47ed8a4ac0c2a97680c3e427f56fa1
kassem00 <kamsdonga@gmail.com>
2024-02-23 22:45:47 +0000

    au

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -18,4 +18,4 @@ class State(Base):
     """
     __tablename__ = "states"
     id = Column(Integer, primary_key=True, autoincrement=True)
-    name = Column("name", String(50)) 
+    name = Column(String(128), nullable=False) 
