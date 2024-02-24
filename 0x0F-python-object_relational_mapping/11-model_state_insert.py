4392d218354f2d124e725a8c1cc5697dc87ebf93
kassem00 <kamsdonga@gmail.com>
2024-02-24 03:42:19 +0000

    au

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -6,23 +6,16 @@ from sqlalchemy import create_engine, String, Column, Integer, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import sys
-from relationship_state import State
-from relationship_city import Base, City
 
-
-def insert_state(username, password, database_name):
-    engine = create_engine(f"mysql+mysqldb://{username}:{password}@localhost:3306/{database_name}", pool_pre_ping=True)
-    Base.metadata.create_all(engine)
-    Session = sessionmaker(bind=engine)
-    session = Session()
-    new_state = State(name="Louisiana")
-    session.add(new_state)
-    session.commit()
-    print(new_state.id)
 
 if __name__ == "__main__":
-    if len(sys.argv) != 4:
-        print("Usage: ./11-model_state_insert.py <mysql username> <mysql password> <database name>")
-        sys.exit(1)
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
 
-    insert_state(sys.argv[1], sys.argv[2], sys.argv[3])
+    louisiana = State(name="Louisiana")
+    session.add(louisiana)
+    session.commit()
+    print(louisiana.id)
