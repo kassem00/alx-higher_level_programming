1954dfae4a17a7b8ef034a43e8df60ac4eea0cd8
kassem00 <kamsdonga@gmail.com>
2024-02-24 03:39:00 +0000

    au

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -10,14 +10,19 @@ from relationship_state import State
 from relationship_city import Base, City
 
 
-if __name__ == "__main__":
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
-                           pool_pre_ping=True)
+def insert_state(username, password, database_name):
+    engine = create_engine(f"mysql+mysqldb://{username}:{password}@localhost:3306/{database_name}", pool_pre_ping=True)
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-
     new_state = State(name="Louisiana")
     session.add(new_state)
     session.commit()
+    print(new_state.id)
+
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        print("Usage: ./11-model_state_insert.py <mysql username> <mysql password> <database name>")
+        sys.exit(1)
+
+    insert_state(sys.argv[1], sys.argv[2], sys.argv[3])
