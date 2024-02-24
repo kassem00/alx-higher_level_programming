a1be69f1af4e5345f81c8fec6782f92be1ef9e9f
kassem00 <kamsdonga@gmail.com>
2024-02-24 02:05:02 +0000

    au

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-
+script that lists all State objects from the database hbtn_0e_6_usa
 """
 from sqlalchemy import create_engine, String, Column, Integer, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
