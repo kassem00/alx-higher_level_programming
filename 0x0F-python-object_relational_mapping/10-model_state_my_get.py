895f87b6ffb04d04e23b190ba122bf52d85d015a
kassem00 <kamsdonga@gmail.com>
2024-02-24 03:05:15 +0000

    au

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+script that lists all State objects from the database hbtn_0e_6_usa
+"""
+from sqlalchemy import create_engine, String, Column, Integer, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+import sys
+
+
+from model_state import State
+
+if __name__ == "__main__":
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    st = session.query(State)
+    state = st.filter(State.name.like(sys.argv[4])).order_by(State.id).all()
+
+    for r in state:
+        print(f"{r.id}")
