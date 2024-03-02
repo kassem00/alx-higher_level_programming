57ff3289f4115cc68fcd71fd0ec2d8991378f27f
kassem00 <kamsdonga@gmail.com>
2024-03-02 01:19:33 +0000

    au

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
-from urllib import request
 """
 Python script that fetches https://alx-intranet.hbtn.io/status
 """
+from urllib import request
+
 if __name__ == "__main__":
-	req = request
-	
-	with req.urlopen("https://alx-intranet.hbtn.io/status") as resp:
-	    data = resp.read()
-	
-	print("Body response:")
-	print(f"    - type: {type(data)}")
-	print(f"    - content: {data}")
-	print("    - utf8 content: {}".format(data.decode("utf-8")))
+    req = request
+
+    with req.urlopen("https://alx-intranet.hbtn.io/status") as resp:
+        data = resp.read()
+
+    print("Body response:")
+    print(f"    - type: {type(data)}")
+    print(f"    - content: {data}")
+    print("    - utf8 content: {}".format(data.decode("utf-8")))
