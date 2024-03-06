90f51a3182a76e0748d44aea741f49402809583c
kassem00 <kamsdonga@gmail.com>
2024-03-06 01:40:50 +0000

    au

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -6,9 +6,8 @@ from urllib import request, parse
 import sys
 
 if __name__ == "__main__":
-    url = sys.argv[1]
     data = parse.urlencode({"email": sys.argv[2]}).encode("ascii")
 
-    req = request.Request(url, data)
-    with request.urlopen(req) as response:
+    reqs = request.Request(sys.argv[1], data)
+    with request.urlopen(reqs) as response:
         print(response.read().decode("utf-8"))
