267ee417347b12a78026ee663629587c8226f567
kassem00 <kamsdonga@gmail.com>
2024-03-27 14:36:34 -0700

    au

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,1 +1,9 @@
 #!/usr/bin/node
+const request = require('request');
+request(process.argv[2], function (error, response, body) {
+  if (error) {
+    console.error('Error:', error);
+    return;
+  }
+  console.log('code:', response.statusCode);
+});
