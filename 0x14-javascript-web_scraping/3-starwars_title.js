800e7cbeb25382a0405457d2f110837854f83884
kassem00 <kamsdonga@gmail.com>
2024-03-27 15:06:28 -0700

    au

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 const request = require('request');
-let eps = `https://swapi-api.alx-tools.com/api/films/${process.argv[2]}/`;
+const eps = `https://swapi-api.alx-tools.com/api/films/${process.argv[2]}/`;
 request(eps, function (error, response, body) {
   if (error) {
     console.error('Error:', error);
     return;
   }
-    data = JSON.parse(body);
-    console.log(data.title);
+  const data = JSON.parse(body);
+  console.log(data.title);
 });
