bf7acc0e14fe00421f0fa5d1159f436323746e9d
kassem00 <kamsdonga@gmail.com>
2024-03-26 17:32:44 -0700

    au

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,16 +1,16 @@
 #!/usr/bin/node
-// read from file
-
+// read file
 const fs = require('fs').promises;
-const filename = process.argv[2];
 
 async function readFile () {
+  const argument = process.argv[2];
+
   try {
-    const data = await fs.readFile(filename, { encoding: 'utf8' });
-    console.log(data); // Log the file content
+    const content = await fs.readFile(argument, { encoding: 'utf8' });
+    console.log(content);
   } catch (err) {
     if (err.code === 'ENOENT') {
-      console.error(`Error: The file "${filename}" does not exist.`);
+      console.error(`Error: The file "${argument}" does not exist.`);
     } else {
       console.error('Error reading file:', err);
     }
