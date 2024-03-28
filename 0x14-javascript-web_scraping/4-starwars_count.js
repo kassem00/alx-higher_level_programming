63c352c3812eb78dfb7c2c6eedfbf9ff6251421a
kassem00 <kamsdonga@gmail.com>
2024-03-27 18:07:26 -0700

    au

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -3,10 +3,13 @@ const request = require('request');
 request(process.argv[2], function (error, response, body) {
   if (!error) {
     const results = JSON.parse(body).results;
-    console.log(results.reduce(function(count, movie) {
-      return movie.characters.find(function(character) {
+
+    console.log(results.reduce(function (count, movie) {
+      return movie.characters.find(function (character) {
         return character.endsWith('/18/');
-      }) ? count + 1 : count;
+      })
+        ? count + 1
+        : count;
     }, 0));
   }
 });
