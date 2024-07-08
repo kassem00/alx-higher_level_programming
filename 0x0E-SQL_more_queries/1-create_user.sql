9be8812c4f4733caed76fa3e689285eb822010b0
kassem00 <kamsdonga@gmail.com>
2024-07-08 07:58:46 +0000

    au

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,5 +1,4 @@
--- Creates DB IF NOT EXISTS  hbtn_0c_0.
--- Creates the user user_0d_1 with all privileges.
+-- script that creates the MySQL server user user_0d_1
 CREATE USER
     IF NOT EXISTS 'user_0d_1'@'localhost'
     IDENTIFIED BY 'user_0d_1_pwd';
