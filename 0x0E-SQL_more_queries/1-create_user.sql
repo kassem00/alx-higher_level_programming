7f25e515ac4573f1dc568197dfb6ca4664e9dcc5
kassem00 <kamsdonga@gmail.com>
2024-07-08 07:58:08 +0000

    au

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,2 +1,10 @@
 -- Creates DB IF NOT EXISTS  hbtn_0c_0.
-CREATE DATABASE IF NOT EXISTS `hbtn_0c_0`;
+-- Creates the user user_0d_1 with all privileges.
+CREATE USER
+    IF NOT EXISTS 'user_0d_1'@'localhost'
+    IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES
+   ON *.*
+   TO 'user_0d_1'@'localhost'
+   IDENTIFIED BY 'user_0d_1_pwd';
+FLUSH PRIVILEGES;
