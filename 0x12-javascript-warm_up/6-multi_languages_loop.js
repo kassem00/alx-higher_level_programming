d60b65a9f11ece4fad231fc1f94b12cb69f0f015
kassem00 <kamsdonga@gmail.com>
2025-03-19 21:08:14 +0200

    au

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,3 +1,10 @@
 #!/usr/bin/node
-// script that prints two arguments passed to it, in the following format: “ is ”
-console.log(`${process.argv[2]} is ${process.argv[3]}`);
+// Script that prints 3 lines using an array and a loop
+const languages = ["C is fun", "Python is cool", "JavaScript is amazing"];
+let output = "";
+
+for (let i = 0; i < languages.length; i++) {
+  output += languages[i] + "\n";
+}
+
+console.log(output.trim());
