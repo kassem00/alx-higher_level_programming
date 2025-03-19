d53526e787665fdade2926df2804841165dc9b77
kassem00 <kamsdonga@gmail.com>
2025-03-19 21:10:51 +0200

    au

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -2,12 +2,13 @@
 // script print number of c is fun u have to pass number
 let arg = Number(process.argv[2]);
 
+
 if (arg == NaN) {
   console.log("Missing number of occurrences");
 } else {
-
-  while (arg) {
-    console.log("C is fun");
-    arg--;
-  }
+  if (arg > 0)
+    while (arg) {
+      console.log("C is fun");
+      arg--;
+    }
 }
