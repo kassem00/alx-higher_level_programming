a83a3682611f12cd5824ac18a0af58b8f95703c4
kassem00 <kamsdonga@gmail.com>
2025-03-21 03:04:27 +0200

    au

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
-// script print squar size define by num u print 
-let arg = Number(process.argv[2]);
+// script print squar size define by num u print
+const arg = Number(process.argv[2]);
 
 if (isNaN(arg) || arg === undefined) {
-  console.log("Missing size");
+  console.log('Missing size');
 } else {
   for (let i = arg; i > 0; i--) {
     console.log('X'.repeat(arg));
