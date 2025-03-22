c4d2fd74536f9c794f551123f713c6da3f84b39c
kassem00 <kamsdonga@gmail.com>
2025-03-22 10:49:05 +0200

    au

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
 // Class Square that inherits from Square of 5-square.js
 exports.nbOccurences = function (list, searchElement) {
-  let last_found = list.indexOf(searchElement);
+  let LastFound = list.indexOf(searchElement);
   let counter = 0;
 
-  while (last_found != -1) {
+  while (LastFound !== -1) {
     counter++;
-    last_found = list.indexOf(searchElement, last_found + 1);
+    LastFound = list.indexOf(searchElement, LastFound + 1);
   }
   return counter;
-}
+};
