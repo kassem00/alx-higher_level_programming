6e41f381247b53cabc5a99886eeaa6033313d475
kassem00 <kamsdonga@gmail.com>
2025-03-23 09:20:15 +0200

    au

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,1 +1,24 @@
 #!/usr/bin/node
+const request = require('request');
+
+request('https://jsonplaceholder.typicode.com/todos', function (error, response, body) {
+  if (error) {
+    console.error(`Error: ${error.message}`);
+    return;
+  }
+  if (response.statusCode !== 200) {
+    console.error(`Failed with status code: ${response.statusCode}`);
+    return;
+  }
+
+  const todos = JSON.parse(body);
+  const completedByUser = {};
+
+  todos.forEach((todo) => {
+    if (todo.completed) {
+      completedByUser[todo.userId] = (completedByUser[todo.userId] || 0) + 1;
+    }
+  });
+
+  console.log(completedByUser);
+});
