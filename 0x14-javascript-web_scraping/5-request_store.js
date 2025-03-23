bd2b3f167c2e8fb167da1f576b110cd0d51df186
kassem00 <kamsdonga@gmail.com>
2025-03-23 07:15:12 +0200

    au

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,1 +1,22 @@
 #!/usr/bin/node
+// Script that fetches a webpage and stores it in a file
+const request = require('request');
+const fs = require('fs');
+
+if (process.argv.length < 4) {
+  console.error('Usage: ./5-request_store.js <URL> <file_path>');
+} else {
+  request(process.argv[2], (error, response, body) => {
+    if (error) {
+      console.error('Error:', error.message);
+    } else if (response.statusCode !== 200) {
+      console.error(`Failed to fetch URL. Status code: ${response.statusCode}`);
+    } else {
+      fs.writeFile(process.argv[3], body, { encoding: 'utf8' }, (err) => {
+        if (err) {
+          console.error('Error writing to file:', err.message);
+        }
+      });
+    }
+  });
+}
