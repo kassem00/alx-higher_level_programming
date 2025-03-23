eadd29d1611c1b1863a42bf9ebef24759a55563c
kassem00 <kamsdonga@gmail.com>
2025-03-23 13:38:48 +0200

    au

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,1 +1,38 @@
 #!/usr/bin/node
+const request = require('request');
+
+if (process.argv.length < 3) {
+  console.error('Usage: ./100-starwars_characters.js <FILM ID>');
+  process.exit(1);
+}
+
+const url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
+
+request(url, (error, response, body) => {
+  if (error) {
+    console.error(`Error: ${error.message}`);
+    process.exit(1);
+  }
+  if (response.statusCode !== 200) {
+    console.error(`Failed with status code: ${response.statusCode}`);
+    process.exit(1);
+  }
+
+  const characters = JSON.parse(body).characters;
+  for (let i = 0; i < characters.length; i++) {
+    const url = characters[i];
+
+    request(url, (error, response, body) => {
+      if (error) {
+        console.error(`Error: ${error.message}`);
+        process.exit(1);
+      }
+      if (response.statusCode !== 200) {
+        console.error(`Failed with status code: ${response.statusCode}`);
+        process.exit(1);
+      }
+      console.log(JSON.parse(body).name);
+    }
+    );
+  }
+});
