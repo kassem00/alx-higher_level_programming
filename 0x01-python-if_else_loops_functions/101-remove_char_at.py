a13bcb2a67272920a994df9db3c1d117d39b5485
kassem00 <kamsdonga@gmail.com>
2025-07-13 17:01:36 +0300

    au

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -5,7 +5,6 @@ def remove_char_at(str, n):
     function that creates a copy of the string, removing the
     character at the position n
     """
-    if n >= 0:
-        n = str[:n] + str[n+1:]
-        return n
-    raise ValueError("Value cannot be negative.")
+    if n >= 0 and n < len(str):
+        return str[:n] + str[n+1:]
+    return str
